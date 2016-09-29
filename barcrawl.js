a188b13852efd3190cecb6f78b9e1c117ca49b56
Ben Clegg <bclegg@midcode.co.uk>
2016-09-29 18:39:02 +0100

    added checkbox

--- barcrawl.js
+++ barcrawl.js
@@ -2,6 +2,20 @@ function linkTo(target) {
     location.href = target;
 }
 
+function showCheckbox(id) {
+    var cbox  = document.getElementById('chk' + id);
+    if (cbox.style.display == 'none') {
+        cbox.style.display = '';
+    }
+}
+
+function hideCheckbox(id) {
+    var cbox  = document.getElementById('chk' + id);
+    if (cbox.style.display != 'none') {
+        cbox.style.display = 'none';
+    }
+}
+
 function markObjective(id) {
     //Objectives should be in format XYY
     //Where X is venue number and YY is objective number
@@ -13,11 +27,13 @@ function markObjective(id) {
             //checked - so unchecked
             localStorage.setItem(lsKey, 0);
             document.getElementById(id).classList.remove('marked');
+            hideCheckbox(id);
         }
         else {
             //unchecked - so check
             localStorage.setItem(lsKey, 1);
             document.getElementById(id).classList.add('marked');
+            showCheckbox(id);
         }
     }
 
@@ -26,21 +42,30 @@ function markObjective(id) {
 function getObjective(id) {
     var lsKey = 'obj' + id;
 
+    var checkbox = document.createElement("span");
+    checkbox.id = 'chk' + id;
+    checkbox.innerText = '✓';
+    document.getElementById(id).firstElementChild.appendChild(checkbox);
+
+
     if(localStorage.getItem(lsKey)) {
         //localStorage exists - check if true or false (1 or 0)
         if(localStorage.getItem(lsKey) == 1) {
             //checked
             document.getElementById(id).classList.add('marked');
+            showCheckbox(id);
         }
         else {
             //unchecked
             document.getElementById(id).classList.remove('marked');
+            hideCheckbox(id);
         }
     }
     else {
         //Does not exist - set as unchecked
         localStorage.setItem(lsKey, 0);
-            document.getElementById(id).classList.remove('marked');
+        document.getElementById(id).classList.remove('marked');
+        hideCheckbox(id);
     }
 }
 
